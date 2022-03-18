190b57d84eb98d4400727d8c0110470dbbcca278
Kaylee Chan <kaychan@chapman.edu>
2022-03-18 14:28:18 -0700

    message

--- mgqReceive.c
+++ mgqReceive.c
@@ -1,23 +1,54 @@
-#define MAX_LINE 80 /* 80 chars per line, per command */
+/* Here's the receiver program. */
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
+#include <errno.h>
+#include <unistd.h>
 
-struct msgbuf {
-long mtype;
-      char mtext[MAX_LINE];
+#include <sys/msg.h>
+
+
+struct my_msg_st {
+    long int my_msg_type;
+    char some_text[BUFSIZ];
 };
-int main(int argc, char *argv[])
+
+int main()
 {
-key_t  key;  /* message queue key */
-   int should_run = 1;
-key = ftok(argv[1], ‘q’);
-/* Create/attach message queue using key */
-    while (should_run) {   
-        
-/** read command from message queue then call the system ( ) 
-function to execute the system command. Break out of loop if 
-‘quit’ or ‘exit’ is read from the queue then exit program **/
-}
-return 0;
+    int running = 1;
+    int msgid;
+    struct my_msg_st some_data;
+    long int msg_to_receive = 0;
+
+/* First, we set up the message queue. */
+
+    msgid = msgget((key_t)1234, 0666 | IPC_CREAT);
+
+    if (msgid == -1) {
+        fprintf(stderr, "msgget failed with error: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+/* Then the messages are retrieved from the queue, until an end message is encountered.
+ Lastly, the message queue is deleted. */
+
+    while(running) {
+        if (msgrcv(msgid, (void *)&some_data, BUFSIZ,
+                   msg_to_receive, 0) == -1) {
+            fprintf(stderr, "msgrcv failed with error: %d\n", errno);
+            exit(EXIT_FAILURE);
+        }
+        printf("You wrote: %s", some_data.some_text);
+        if (strncmp(some_data.some_text, "end", 3) == 0) {
+            running = 0;
+        }
+    }
+
+    if (msgctl(msgid, IPC_RMID, 0) == -1) {
+        fprintf(stderr, "msgctl(IPC_RMID) failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
 }
