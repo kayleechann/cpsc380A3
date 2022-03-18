7f2579fe05af6c026ff1dcfd286d37c4e77cf4ed
Youssef Hegab <hegab@chapman.edu>
2022-03-18 16:58:30 -0700

    hhmmmm very interesting

--- msgqSend.c
+++ msgqSend.c
@@ -1,61 +1,73 @@
-/* The sender program is very similar to msg1.c. In the main set up, delete the
- msg_to_receive declaration and replace it with buffer[BUFSIZ], remove the message
- queue delete and make the following changes to the running loop.
- We now have a call to msgsnd to send the entered text to the queue. */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
+#include <signal.h>
+#include <errno.h>
 #include <sys/msg.h>
 
-#define MAX_TEXT 512
 
-struct my_msg_st {
-    long int my_msg_type;
-    char some_text[MAX_TEXT];
-};
+#define MAX_LINE 80 /* 80 chars per line, per command */
 
-int main(int argc, char *argv[])
-{
-    int running = 1;
-    struct my_msg_st some_data;
-    int msgid;
-    char buffer[BUFSIZ];
+typedef struct {
+    long mtype;
+    char mtext[MAX_LINE];
+} msgbuf_1 ;
 
-    if (argc == 1){
-        printf("Please enter filename\n");
-        exit(1);
+int main(int argc, char *argv[]){
+
+  // exit conditions
+  char q1[5] = "quit";
+  char q2[5] = "exit";
+
+  int should_run = 1;
+
+  msgbuf_1 data;
+  int msgid;
+  char buffer[BUFSIZ];
+
+  key_t key; /* message queue key */
+  key = ftok(argv[1], 'q');
+
+  /* Create/attach message queue using key */
+
+  msgid = msgget(key, 0666 | IPC_CREAT);
+
+  if (msgid == -1) {
+    fprintf(stderr, "msgget error: %d\n", errno);
+    exit(EXIT_FAILURE);
+  }
+
+  while (should_run) {
+    printf("msgq>");
+    fflush(stdout);
+
+    /** read command from stdin then send command to message queue
+    Break out of loop if user types ‘quit’ or ‘exit’ then delete the message queue and exit
+    program **/
+
+    fgets(buffer, BUFSIZ, stdin);
+    data.mtype = 1;
+    strcpy(data.mtext, buffer);
+
+    if (msgsnd(msgid, (void *)&data, MAX_LINE, 0) == -1) {
+      fprintf(stderr, "msgsnd failed\n");
+      exit(EXIT_FAILURE);
+    }
+    if (strncmp(buffer, "end", 3) == 0) {
+      should_run = 0;
     }
 
-    key_t key; /* message queue key */
-    key = ftok(argv[1], 'q');
-
-
-    msgid = msgget(key, 0666 | IPC_CREAT);
-
-    if (msgid == -1) {
-        fprintf(stderr, "msgget failed with error: %d\n", errno);
-        exit(EXIT_FAILURE);
+    //exit loop if user types quit or exit
+    if((strncmp(buffer, q2 , 4) == 0 )| (strncmp(buffer, q1 ,4) == 0)){
+      should_run = 0;
     }
+  }
+  exit(EXIT_SUCCESS);
 
-    while(running) {
-        printf("msgq> ");
-        fflush(stdout);
-        fgets(buffer, BUFSIZ, stdin);
-        some_data.my_msg_type = 1;
-        strcpy(some_data.some_text, buffer);
-
-        if (msgsnd(msgid, (void *)&some_data, MAX_TEXT, 0) == -1) {
-            fprintf(stderr, "msgsnd failed\n");
-            exit(EXIT_FAILURE);
-        }
-        if (strncmp(buffer, "exit", 4) == 0 || strncmp(buffer, "quit", 4) == 0) {
-            running = 0;
-        }
-    }
-
-    exit(EXIT_SUCCESS);
+  return 0;
 }
